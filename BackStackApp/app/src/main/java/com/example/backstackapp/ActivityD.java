5eb9c77a542f5d1abb31cfd7b027ac756fc734c0
Tuğçe Saygın <tugcesaygin.05@gmail.com>
2021-04-29 01:34:15 +0300

    Activity D addFlags aciklandi

--- BackStackApp/app/src/main/java/com/example/backstackapp/ActivityD.java
+++ BackStackApp/app/src/main/java/com/example/backstackapp/ActivityD.java
@@ -17,7 +17,7 @@ public class ActivityD extends AppCompatActivity {
     public void onBackPressed() {
         super.onBackPressed();
         Intent intent = new Intent(ActivityD.this,ActivityB.class);
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP); // D'den B'ye gectikten sonra geri basinca oto C'ye geciyodu bu 2 kod satiri sayesinde A B C D B A diye gidiyor.
         startActivity(intent);
     }
 }
