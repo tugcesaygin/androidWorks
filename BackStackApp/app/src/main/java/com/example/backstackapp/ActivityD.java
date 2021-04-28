c78ec4fd956f19f0ba06d531cce4b312cb02e3ce
Tuğçe Saygın <tugcesaygin.05@gmail.com>
2021-04-29 01:00:08 +0300

    main addedd

---
+++ BackStackApp/app/src/main/java/com/example/backstackapp/ActivityD.java
@@ -0,0 +1,14 @@
+package com.example.backstackapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityD extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_d);
+    }
+}
